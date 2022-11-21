3442552cd2441c62cd6954113ca8171e621bae01
GabrielSCorrea <correagabriel072@gmail.com>
2022-11-21 00:44:09 +0000

    Changing User model

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -4,4 +4,12 @@ from base.models import User, Suggestion
 class SuggestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Suggestion
-        fields: '__all__'
+        fields = '__all__'
+        
+        
+        
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+        
--- backend/base/models.py
+++ backend/base/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 
 class User(models.Model):
-    name = models.CharField(max_length=30)
+    id= models.CharField(primary_key=True, max_length=150, unique=True)
+    username = models.CharField(max_length=150)    
+    email = models.EmailField(default="email field", unique=True)
     created = models.DateTimeField(auto_now_add=True)
+    is_staff = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.username
+    
 
 class Suggestion(models.Model):
-    user = models.ForeignKey("User", models.SET_NULL, null=True)
-    suggestion = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    suggestion = models.TextField()
     created =  models.DateTimeField(auto_now_add=True)
     
 
