42835294554ebff4297035cd20e607be39389ab8
nunonuness <nunoguilherme8@gmail.com>
2022-11-28 01:12:19 +0000

    Views
    register view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,9 @@
 from rest_framework.response import Response
+from rest_framework import generics, permissions
+from knox.models import AuthToken
 from rest_framework.decorators import api_view
 from base.models import Suggestion, User
-from .serializers import SuggestionSerializer
+from .serializers import SuggestionSerializer, RegisterSerializer, UserSerializer
 
 @api_view(['GET'])
 def getSuggestions(request):
@@ -17,16 +19,14 @@ def getSuggestion(request, pk):
     return Response(serializer.data)
 
 @api_view(['POST'])
-def postSuggestions(request):
-    data = request.data
-    
-    suggestion = Suggestion.objects.create(
-        user = data['user'],
-        suggestion= data['suggestion'],
-    )
-    
-    serializer = SuggestionSerializer(suggestion, many = False)
-    if serializer.is_valid():
-        serializer.save()
-        
-    return Response(serializer.data)
+class RegisterAPI(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+        "user": UserSerializer(user, context=self.get_serializer_context()).data,
+        "token": AuthToken.objects.create(user)[1]
+        })
